5524e40d75de4957e35861f106c7957c506f696f
zzj <2998637256@qq.com>
2022-10-26 11:00:45 +0800

    chore:modify the value of the salt

--- src/main/java/cn/programerzhang/utils/TokenUtil.java
+++ src/main/java/cn/programerzhang/utils/TokenUtil.java
@@ -13,13 +13,13 @@ public class TokenUtil {
         map.put("content", content);
         String token = jwtBuilder.setClaims(map) //通过map传值
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) //设置token有效期
-                .signWith(SignatureAlgorithm.HS256, "zhangzijia").compact();//设置token加密方式和密码
+                .signWith(SignatureAlgorithm.HS256, "xilinba").compact();//设置token加密方式和密码
         return token;
     }
 
     public static String parseToken(String token) {
         JwtParser jwtParser = Jwts.parser(); //获取jwt解析器
-        jwtParser.setSigningKey("zhangzijia");
+        jwtParser.setSigningKey("xilinba");
         Jws<Claims> claimsJws = jwtParser.parseClaimsJws(token);
         Claims body = claimsJws.getBody();//获取body
         String content = body.get("content", String.class);
